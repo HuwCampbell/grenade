68199648f4380e897faf2896f2f991b575dbc443
Huw Campbell <huw.campbell@ambiata.com>
2017-12-14 21:29:45 +1100

    Remove old functions

--- test/Test/Hedgehog/Compat.hs
+++ test/Test/Hedgehog/Compat.hs
@@ -3,28 +3,19 @@ module Test.Hedgehog.Compat (
     (...)
   , choose
   , blindForAll
-  , semiBlindForAll
-  , forAllRender
   )where
 
 import           Hedgehog (Gen)
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import           Hedgehog.Internal.Property
-import           Hedgehog.Internal.Source
 
 (...) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
 (...) = (.) . (.)
 {-# INLINE (...) #-}
 
-choose :: ( Integral a ) => a -> a -> Gen a
+choose :: Integral a => a -> a -> Gen a
 choose = Gen.integral ... Range.constant
 
 blindForAll :: Monad m => Gen a -> PropertyT m a
 blindForAll = forAllWith (const "blind")
-
-semiBlindForAll :: (Monad m, Show a, HasCallStack) => Gen a -> PropertyT m a
-semiBlindForAll = forAllWith (const "blind")
-
-forAllRender :: (Monad m, HasCallStack) => ( a -> String ) -> Gen a -> PropertyT m a
-forAllRender render gen = forAllWith render gen
