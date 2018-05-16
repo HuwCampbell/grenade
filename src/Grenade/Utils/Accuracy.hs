7d6c5449e9699416d482cdfd6dd08ac175f34c69
Nick Van den Broeck <nick.van.den.broeck666@gmail.com>
2018-05-16 16:15:07 +0200

    Made changes based on comments

--- src/Grenade/Utils/Accuracy.hs
+++ src/Grenade/Utils/Accuracy.hs
@@ -1,51 +1,20 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Grenade.Utils.Accuracy
-    ( Accuracy
-    , accuracyM
-    , showAccuracy
+    ( ClassificationAccuracy
+    , showClassificationAccuracy
+    , constructProperFraction
     ) where
 
-import Data.Validity
+import Grenade.Utils.ProperFraction
 
-import Data.Aeson (FromJSON, ToJSON)
+-- | Classification accuracy of a network on a dataset.
+type ClassificationAccuracy = ProperFraction
 
-import GHC.Generics
-
-import Control.Monad.Catch
-
-newtype Accuracy =
-    Accuracy Double
-    deriving (Show, Eq, Generic)
-
-data AccuracyOutOfBounds =
-    AccuracyOutOfBounds
-    deriving (Show, Eq, Generic)
-
-instance Exception AccuracyOutOfBounds where
-    displayException = const "The accuracy is not in [0,1]."
-
-accuracyM :: MonadThrow m => Double -> m Accuracy
-accuracyM x =
-    case prettyValidation $ Accuracy x of
-        Right a -> pure a
-        Left _ -> throwM AccuracyOutOfBounds
-
-instance ToJSON Accuracy
-
-instance FromJSON Accuracy
-
-instance Validity Accuracy where
-    validate (Accuracy x) =
-        mconcat
-            [ delve "The accuracy contains a valid double" x
-            , declare "The accuracy is positive" $ x >= 0
-            , declare "The accuracy is smaller than 1" $ x <= 1
-            ]
-
-showAccuracy :: String -> Accuracy -> String
-showAccuracy name (Accuracy x) =
-    "The " ++ name ++ " accuracy is " ++ show (nOfPrecent * x) ++ "%."
+showClassificationAccuracy :: String -> ClassificationAccuracy -> String
+showClassificationAccuracy name acc =
+    "The " ++
+    name ++ " accuracy is " ++ show (nOfPrecent * properToDouble acc) ++ "%."
 
 nOfPrecent :: Double
 nOfPrecent = 100
