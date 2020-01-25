3d39f38cc73c18e63fac2e74aca1ffa307cb267b
jrp2014 <jrp2014@users.noreply.github.com>
2020-01-25 14:16:40 +0000

    Work in progress

--- examples/main/mnist.hs
+++ examples/main/mnist.hs
@@ -11,15 +11,19 @@ import           Control.Monad
 import           Control.Monad.Random
 import           Control.Monad.Trans.Except
 
-import qualified Data.Attoparsec.Text as A
-import           Data.List ( foldl' )
-import           Data.Semigroup ( (<>) )
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import qualified Data.Vector.Storable as V
+import qualified Data.Attoparsec.Text          as A
+import           Data.List                      ( foldl' )
+import           Data.Semigroup                 ( (<>) )
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
+import qualified Data.Vector.Storable          as V
 
-import           Numeric.LinearAlgebra ( maxIndex )
-import qualified Numeric.LinearAlgebra.Static as SA
+import           Codec.Compression.GZip         ( decompress )
+import qualified Data.ByteString.Lazy          as BS
+import           GHC.Int                        ( Int64 )
+
+import           Numeric.LinearAlgebra          ( maxIndex )
+import qualified Numeric.LinearAlgebra.Static  as SA
 
 import           Options.Applicative
 
@@ -30,10 +34,7 @@ import           Grenade.Utils.OneHot
 --
 -- This network is used to show how we can embed a Network as a layer in the larger MNIST
 -- type.
-type FL i o =
-  Network
-    '[ FullyConnected i o, Logit ]
-    '[ 'D1 i, 'D1 o, 'D1 o ]
+type FL i o = Network '[FullyConnected i o, Logit] '[ 'D1 i, 'D1 o, 'D1 o]
 
 -- The definition of our convolutional neural network.
 -- In the type signature, we have a type level list of shapes which are passed between the layers.
@@ -48,69 +49,117 @@ type FL i o =
 -- /NOTE:/ This model is actually too complex for MNIST, and one should use the type given in the readme instead.
 --         This one is just here to demonstrate Inception layers in use.
 --
-type MNIST =
-  Network
-    '[ Reshape,
-       Concat ('D3 28 28 1) Trivial ('D3 28 28 14) (InceptionMini 28 28 1 5 9),
-       Pooling 2 2 2 2, Relu,
-       Concat ('D3 14 14 3) (Convolution 15 3 1 1 1 1) ('D3 14 14 15) (InceptionMini 14 14 15 5 10), Crop 1 1 1 1, Pooling 3 3 3 3, Relu,
-       Reshape, FL 288 80, FL 80 10 ]
-    '[ 'D2 28 28, 'D3 28 28 1,
-       'D3 28 28 15, 'D3 14 14 15, 'D3 14 14 15, 'D3 14 14 18,
-       'D3 12 12 18, 'D3 4 4 18, 'D3 4 4 18,
-       'D1 288, 'D1 80, 'D1 10 ]
+type MNIST
+  = Network
+      '[Reshape, Concat
+        ( 'D3 28 28 1)
+        Trivial
+        ( 'D3 28 28 14)
+        (InceptionMini 28 28 1 5 9), Pooling 2 2 2 2, Relu, Concat
+        ( 'D3 14 14 3)
+        (Convolution 15 3 1 1 1 1)
+        ( 'D3 14 14 15)
+        (InceptionMini 14 14 15 5 10), Crop 1 1 1 1, Pooling 3 3 3 3, Relu, Reshape, FL
+        288
+        80, FL 80 10]
+      '[ 'D2 28 28, 'D3 28 28 1, 'D3 28 28 15, 'D3 14 14 15, 'D3 14 14 15, 'D3
+        14
+        14
+        18, 'D3 12 12 18, 'D3 4 4 18, 'D3 4 4 18, 'D1 288, 'D1 80, 'D1 10]
 
 randomMnist :: MonadRandom m => m MNIST
 randomMnist = randomNetwork
 
-convTest :: Int -> FilePath -> FilePath -> LearningParameters -> ExceptT String IO ()
-convTest iterations trainFile validateFile rate = do
-  net0         <- lift randomMnist
-  trainData    <- readMNIST trainFile
-  validateData <- readMNIST validateFile
-  lift $ foldM_ (runIteration trainData validateData) net0 [1..iterations]
+convTest :: Int -> FilePath -> LearningParameters -> ExceptT String IO ()
+convTest iterations dataDir rate = do
+  net0 <- lift randomMnist
 
-    where
+  let trainData = readMNIST trainI trainL
+  let validateData = readMNIST validateI validateL
+
+  lift $ foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
+
+ where
+
+  [trainI, trainL, validateI, validateL] =
+    ((decompress <$>) . BS.readFile . ((dataDir ++ "/") ++)) <$>
+    [ "train-images-idx3-ubyte.gz"
+    , "train-labels-idx1-ubyte.gz"
+    , "t10k-images-idx3-ubyte.gz"
+    , "t10k-labels-idx1-ubyte.gz"
+    ]
   trainEach rate' !network (i, o) = train rate' network i o
 
   runIteration trainRows validateRows net i = do
-    let trained' = foldl' (trainEach ( rate { learningRate = learningRate rate * 0.9 ^ i} )) net trainRows
-    let res      = fmap (\(rowP,rowL) -> (rowL,) $ runNet trained' rowP) validateRows
-    let res'     = fmap (\(S1D label, S1D prediction) -> (maxIndex (SA.extract label), maxIndex (SA.extract prediction))) res
+    let trained' = foldl'
+          (trainEach (rate { learningRate = learningRate rate * 0.9 ^ i }))
+          net
+          trainRows
+    let res =
+          fmap (\(rowP, rowL) -> (rowL, ) $ runNet trained' rowP) validateRows
+    let res' = fmap
+          (\(S1D label, S1D prediction) ->
+            (maxIndex (SA.extract label), maxIndex (SA.extract prediction))
+          )
+          res
     print trained'
-    putStrLn $ "Iteration " ++ show i ++ ": " ++ show (length (filter ((==) <$> fst <*> snd) res')) ++ " of " ++ show (length res')
+    putStrLn
+      $  "Iteration "
+      ++ show i
+      ++ ": "
+      ++ show (length (filter ((==) <$> fst <*> snd) res'))
+      ++ " of "
+      ++ show (length res')
     return trained'
 
-data MnistOpts = MnistOpts FilePath FilePath Int LearningParameters
+data MnistOpts = MnistOpts FilePath Int LearningParameters
 
 mnist' :: Parser MnistOpts
-mnist' = MnistOpts <$> argument str (metavar "TRAIN")
-                   <*> argument str (metavar "VALIDATE")
-                   <*> option auto (long "iterations" <> short 'i' <> value 15)
-                   <*> (LearningParameters
-                       <$> option auto (long "train_rate" <> short 'r' <> value 0.01)
-                       <*> option auto (long "momentum" <> value 0.9)
-                       <*> option auto (long "l2" <> value 0.0005)
-                       )
+mnist' =
+  MnistOpts
+    <$> argument str (metavar "DATADIR")
+    <*> option auto (long "iterations" <> short 'i' <> value 15)
+    <*> (   LearningParameters
+        <$> option auto (long "train_rate" <> short 'r' <> value 0.01)
+        <*> option auto (long "momentum" <> value 0.9)
+        <*> option auto (long "l2" <> value 0.0005)
+        )
 
 main :: IO ()
 main = do
-    MnistOpts mnist vali iter rate <- execParser (info (mnist' <**> helper) idm)
-    putStrLn "Training convolutional neural network..."
+  MnistOpts mnistDir iter rate <- execParser (info (mnist' <**> helper) idm)
+  putStrLn "Training convolutional neural network..."
 
-    res <- runExceptT $ convTest iter mnist vali rate
-    case res of
-      Right () -> pure ()
-      Left err -> putStrLn err
+  res <- runExceptT $ convTest iter mnistDir rate
+  case res of
+    Right ()  -> pure ()
+    Left  err -> putStrLn err
 
-readMNIST :: FilePath -> ExceptT String IO [(S ('D2 28 28), S ('D1 10))]
-readMNIST mnist = ExceptT $ do
-  mnistdata <- T.readFile mnist
-  return $ traverse (A.parseOnly parseMNIST) (T.lines mnistdata)
+--readMNIST
+--  :: [BS.ByteString]
+--  -> [BS.ByteString]
+--  -> ExceptT String IO [(S ( 'D2 28 28), S ( 'D1 10))]
+readMNIST images labels = ExceptT $ do
+  n <- [0 ..]
+  return $ parseMNIST images labels n
 
-parseMNIST :: A.Parser (S ('D2 28 28), S ('D1 10))
-parseMNIST = do
-  Just lab <- oneHot <$> A.decimal
-  pixels   <- many (A.char ',' >> A.double)
-  image    <- maybe (fail "Parsed row was of an incorrect size") pure (fromStorable . V.fromList $ pixels)
+parseMNIST
+  :: BS.ByteString -> BS.ByteString -> Int64 -> (S ( 'D2 28 28), S ( 'D1 10))
+parseMNIST si sl n = do
+  lab    <- getLabel sl n
+  pixels <- getImage si n
+  image  <- pure ((/ 255) <$> pixels)  -- convert to Fractional 0 .. 1
   return (image, lab)
+
+getImage :: Num b => BS.ByteString -> Int64 -> [b]
+getImage s n =
+  fromIntegral . BS.index s . (n * 28 * 28 + 16 +) <$> [0 .. 28 * 28 - 1] -- 16 = header (ignored)
+
+getX :: Fractional b => BS.ByteString -> Int64 -> [b]
+getX s n = (/ 255) <$> getImage s n
+
+getLabel :: Num b => BS.ByteString -> Int64 -> b
+getLabel s n = fromIntegral $ BS.index s (n + 8) -- 8 = header (ignored)
+
+getY :: Num b => BS.ByteString -> Int64 -> [b]
+getY s n = fromIntegral . fromEnum . (getLabel s n ==) <$> [0 .. 9]
