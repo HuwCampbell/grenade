19c3521be1081c41049af58a16698de1b785592b
jrp2014 <jrp2014@users.noreply.github.com>
2020-01-25 14:16:40 +0000

    Work in progress

--- examples/main/mnist.hs
+++ examples/main/mnist.hs
@@ -13,6 +13,7 @@ import           Control.Monad.Random
 
 import           Codec.Compression.GZip ( decompress )
 import           Data.Serialize ( Get )
+
 import qualified Data.Serialize as Serialize
 import qualified Data.ByteString.Lazy as B
 
@@ -22,6 +23,7 @@ import           Data.Maybe ( fromMaybe )
 #if ! MIN_VERSION_base(4,13,0)
 import           Data.Semigroup ( (<>) )
 #endif
+
 import           Data.Word ( Word32 , Word8 )
 import qualified Data.Vector.Storable as V
 
@@ -38,10 +40,7 @@ import           Grenade.Utils.OneHot
 --
 -- This network is used to show how we can embed a Network as a layer in the larger MNIST
 -- type.
-type FL i o =
-  Network
-    '[ FullyConnected i o, Logit ]
-    '[ 'D1 i, 'D1 o, 'D1 o ]
+type FL i o = Network '[FullyConnected i o, Logit] '[ 'D1 i, 'D1 o, 'D1 o]
 
 -- The definition of our convolutional neural network.
 -- In the type signature, we have a type level list of shapes which are passed between the layers.
@@ -56,21 +55,28 @@ type FL i o =
 -- /NOTE:/ This model is actually too complex for MNIST, and one should use the type given in the readme instead.
 --         This one is just here to demonstrate Inception layers in use.
 --
-type MNIST =
-  Network
-    '[ Reshape,
-       Concat ('D3 28 28 1) Trivial ('D3 28 28 14) (InceptionMini 28 28 1 5 9),
-       Pooling 2 2 2 2, Relu,
-       Concat ('D3 14 14 3) (Convolution 15 3 1 1 1 1) ('D3 14 14 15) (InceptionMini 14 14 15 5 10), Crop 1 1 1 1, Pooling 3 3 3 3, Relu,
-       Reshape, FL 288 80, FL 80 10 ]
-    '[ 'D2 28 28, 'D3 28 28 1,
-       'D3 28 28 15, 'D3 14 14 15, 'D3 14 14 15, 'D3 14 14 18,
-       'D3 12 12 18, 'D3 4 4 18, 'D3 4 4 18,
-       'D1 288, 'D1 80, 'D1 10 ]
+type MNIST
+  = Network
+      '[Reshape, Concat
+        ( 'D3 28 28 1)
+        Trivial
+        ( 'D3 28 28 14)
+        (InceptionMini 28 28 1 5 9), Pooling 2 2 2 2, Relu, Concat
+        ( 'D3 14 14 3)
+        (Convolution 15 3 1 1 1 1)
+        ( 'D3 14 14 15)
+        (InceptionMini 14 14 15 5 10), Crop 1 1 1 1, Pooling 3 3 3 3, Relu, Reshape, FL
+        288
+        80, FL 80 10]
+      '[ 'D2 28 28, 'D3 28 28 1, 'D3 28 28 15, 'D3 14 14 15, 'D3 14 14 15, 'D3
+        14
+        14
+        18, 'D3 12 12 18, 'D3 4 4 18, 'D3 4 4 18, 'D1 288, 'D1 80, 'D1 10]
 
 randomMnist :: MonadRandom m => m MNIST
 randomMnist = randomNetwork
 
+<<<<<<< HEAD
 convTest :: Int -> FilePath -> Maybe Int -> LearningParameters -> IO ()
 convTest iterations dataDir nSamples rate = do
   net0         <- randomMnist
@@ -80,11 +86,30 @@ convTest iterations dataDir nSamples rate = do
                             (dataDir </> "t10k-labels-idx1-ubyte.gz")
 
   foldM_ (runIteration (maybe trainData (`take` trainData) nSamples) validateData) net0 [1..iterations]
+=======
+convTest :: Int -> FilePath -> LearningParameters -> IO ()
+convTest iterations dataDir rate = do
+  net0      <- lift randomMnist
 
-    where
+
+  trainData <- loadMNIST (mkFilePath "train-images-idx3-ubyte.gz")
+                         (mkFilePath "train-labels-idx1-ubyte.gz")
+
+  validateData <- loadMNIST (mkFilePath "t10k-images-idx3-ubyte.gz")
+                            (mkFilePath "t10k-labels-idx1-ubyte.gz")
+
+  lift $ foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
+
+ where
+
+  mkFilePath :: String -> FilePath
+  mkFilePath s = dataDir ++ '/' : s
+>>>>>>> Work in progress
+
   trainEach rate' !network (i, o) = train rate' network i o
 
   runIteration trainRows validateRows net i = do
+<<<<<<< HEAD
     let trained' = foldl' (trainEach ( rate { learningRate = learningRate rate * 0.9 ^ i} )) net trainRows
     print trained'
 
@@ -130,10 +155,63 @@ readMNIST iFP lFP = do
   return $ zip
     (fmap (fromMaybe (error "bad samples") . fromStorable) samples)
     (fromMaybe (error "bad labels") . oneHot . fromIntegral <$> labels)
+=======
+    let trained' = foldl'
+          (trainEach (rate { learningRate = learningRate rate * 0.9 ^ i }))
+          net
+          trainRows
+    let res =
+          fmap (\(rowP, rowL) -> (rowL, ) $ runNet trained' rowP) validateRows
+    let res' = fmap
+          (\(S1D label, S1D prediction) ->
+            (maxIndex (SA.extract label), maxIndex (SA.extract prediction))
+          )
+          res
+    print trained'
+    putStrLn
+      $  "Iteration "
+      ++ show i
+      ++ ": "
+      ++ show (length (filter ((==) <$> fst <*> snd) res'))
+      ++ " of "
+      ++ show (length res')
+    return trained'
+
+data MnistOpts = MnistOpts FilePath Int LearningParameters
+
+mnist' :: Parser MnistOpts
+mnist' =
+  MnistOpts
+    <$> argument str (metavar "DATADIR")
+    <*> option auto (long "iterations" <> short 'i' <> value 15)
+    <*> (   LearningParameters
+        <$> option auto (long "train_rate" <> short 'r' <> value 0.01)
+        <*> option auto (long "momentum" <> value 0.9)
+        <*> option auto (long "l2" <> value 0.0005)
+        )
+
+main :: IO ()
+main = do
+  MnistOpts mnistDir iter rate <- execParser (info (mnist' <**> helper) idm)
+  putStrLn "Training convolutional neural network..."
+
+  res <- runMaybeT $ convTest iter mnistDir rate
+  case res of
+    Just () -> pure ()
+    Nothing -> putStrLn "Failed"
+
+loadMNIST :: FilePath -> FilePath -> IO (Maybe [(S ( 'D2 28 28), S ( 'D1 10))])
+loadMNIST iFP lFP =  do
+  let labels  = MaybeT $ readMNISTLabels lFP
+  let samples = MaybeT $ fromIntegral . readMNISTSamples $ iFP
+  d <- MaybeT (fromStorable samples, (oneHot . fromIntegral) <$> labels)
+  return d
+>>>>>>> Work in progress
 
 -- | Check's the file's endianess, throwing an error if it's not as expected.
 checkEndian :: Get ()
 checkEndian = do
+<<<<<<< HEAD
   magic <- Serialize.getWord32be
   when (magic `notElem` ([2049, 2051] :: [Word32]))
     $ error "Expected big endian, but image file is little endian."
@@ -143,11 +221,23 @@ readMNISTSamples :: FilePath -> IO [V.Vector Double]
 readMNISTSamples path = do
   raw <- decompress <$> B.readFile path
   either fail ( return . fmap (V.map normalize) ) $ Serialize.runGetLazy getMNIST raw
+=======
+  magic <- getWord32be
+  when (magic `notElem` ([2049, 2051] :: [Word32]))
+    $ fail "Expected big endian, but image file is little endian."
+
+-- | Reads an MNIST file and returns a list of samples.
+readMNISTSamples :: FilePath -> IO [V.Vector Word8]
+readMNISTSamples path = do
+  raw <- decompress <$> BS.readFile path
+  return $ runGet getMNIST raw
+>>>>>>> Work in progress
  where
   getMNIST :: Get [V.Vector Word8]
   getMNIST = do
     checkEndian
     -- Parse header data.
+<<<<<<< HEAD
     cnt    <- fromIntegral <$> Serialize.getWord32be
     rows   <- fromIntegral <$> Serialize.getWord32be
     cols   <- fromIntegral <$> Serialize.getWord32be
@@ -160,18 +250,37 @@ readMNISTSamples path = do
   -- There are other normalization functions in the literature, such as
   -- normalize = (/ 0.3081) . (`subtract` 0.1307) . (/ 255) . fromIntegral
   -- but we need values in the range [0..1] for the showShape' pretty printer
+=======
+    cnt    <- fromIntegral <$> getWord32be
+    rows   <- fromIntegral <$> getWord32be
+    cols   <- fromIntegral <$> getWord32be
+    -- Read all of the data, then split into samples.
+    pixels <- getLazyByteString $ fromIntegral $ cnt * rows * cols
+    return $ V.fromList <$> chunksOf (rows * cols) (BS.unpack pixels)
+>>>>>>> Work in progress
 
 -- | Reads a list of MNIST labels from a file and returns them.
 readMNISTLabels :: FilePath -> IO [Word8]
 readMNISTLabels path = do
+<<<<<<< HEAD
   raw <- decompress <$> B.readFile path
   either fail return $ Serialize.runGetLazy getLabels raw
+=======
+  raw <- decompress <$> BS.readFile path
+  return $ runGet getLabels raw
+>>>>>>> Work in progress
  where
   getLabels :: Get [Word8]
   getLabels = do
     checkEndian
     -- Parse header data.
+<<<<<<< HEAD
     cnt <- fromIntegral <$> Serialize.getWord32be
     -- Read all of the labels.
     B.unpack <$> Serialize.getLazyByteString cnt
+=======
+    cnt <- fromIntegral <$> getWord32be
+    -- Read all of the labels.
+    BS.unpack <$> getLazyByteString cnt
+>>>>>>> Work in progress
 
