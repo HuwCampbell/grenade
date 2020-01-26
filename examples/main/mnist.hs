b544b9301d0775c0a95b50c074c569e589852d62
jrp2014 <jrp2014@users.noreply.github.com>
2020-01-26 13:39:57 +0000

    Adapt MNIST example to use original data files

--- examples/main/mnist.hs
+++ examples/main/mnist.hs
@@ -30,6 +30,7 @@ import qualified Data.Vector.Storable as V
 import           Numeric.LinearAlgebra ( maxIndex )
 import qualified Numeric.LinearAlgebra.Static as SA
 
+
 import           Options.Applicative
 import           System.FilePath ( (</>) )
 
@@ -89,16 +90,15 @@ convTest iterations dataDir nSamples rate = do
 =======
 convTest :: Int -> FilePath -> LearningParameters -> IO ()
 convTest iterations dataDir rate = do
-  net0      <- lift randomMnist
+  net0      <- randomMnist
 
-
-  trainData <- loadMNIST (mkFilePath "train-images-idx3-ubyte.gz")
+  trainData <- readMNIST (mkFilePath "train-images-idx3-ubyte.gz")
                          (mkFilePath "train-labels-idx1-ubyte.gz")
 
-  validateData <- loadMNIST (mkFilePath "t10k-images-idx3-ubyte.gz")
+  validateData <- readMNIST (mkFilePath "t10k-images-idx3-ubyte.gz")
                             (mkFilePath "t10k-labels-idx1-ubyte.gz")
 
-  lift $ foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
+  foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
 
  where
 
@@ -195,11 +195,20 @@ main = do
   MnistOpts mnistDir iter rate <- execParser (info (mnist' <**> helper) idm)
   putStrLn "Training convolutional neural network..."
 
-  res <- runMaybeT $ convTest iter mnistDir rate
-  case res of
-    Just () -> pure ()
-    Nothing -> putStrLn "Failed"
+  convTest iter mnistDir rate
 
+-- Adapted from https://github.com/tensorflow/haskell/blob/master/tensorflow-mnist/src/TensorFlow/Examples/MNIST/Parse.hs
+-- Could also have used Data.IDX, although that uses a different Vector variant from that need for fromStorable
+
+readMNIST :: FilePath -> FilePath -> IO [(S ( 'D2 28 28), S ( 'D1 10))]
+readMNIST iFP lFP = do
+  labels  <- readMNISTLabels lFP
+  samples <- readMNISTSamples iFP
+  return $ zip
+    (fmap (fromMaybe (error "bad samples") . fromStorable) samples)
+    ((fromMaybe (error "bad labels") . oneHot . fromIntegral) <$> labels)
+
+<<<<<<< HEAD
 loadMNIST :: FilePath -> FilePath -> IO (Maybe [(S ( 'D2 28 28), S ( 'D1 10))])
 loadMNIST iFP lFP =  do
   let labels  = MaybeT $ readMNISTLabels lFP
@@ -207,6 +216,8 @@ loadMNIST iFP lFP =  do
   d <- MaybeT (fromStorable samples, (oneHot . fromIntegral) <$> labels)
   return d
 >>>>>>> Work in progress
+=======
+>>>>>>> Adapt MNIST example to use original data files
 
 -- | Check's the file's endianess, throwing an error if it's not as expected.
 checkEndian :: Get ()
@@ -227,11 +238,15 @@ readMNISTSamples path = do
     $ fail "Expected big endian, but image file is little endian."
 
 -- | Reads an MNIST file and returns a list of samples.
-readMNISTSamples :: FilePath -> IO [V.Vector Word8]
+readMNISTSamples :: FilePath -> IO [V.Vector Double]
 readMNISTSamples path = do
   raw <- decompress <$> BS.readFile path
+<<<<<<< HEAD
   return $ runGet getMNIST raw
 >>>>>>> Work in progress
+=======
+  return . fmap (V.map normalize) $ runGet getMNIST raw
+>>>>>>> Adapt MNIST example to use original data files
  where
   getMNIST :: Get [V.Vector Word8]
   getMNIST = do
@@ -258,6 +273,10 @@ readMNISTSamples path = do
     pixels <- getLazyByteString $ fromIntegral $ cnt * rows * cols
     return $ V.fromList <$> chunksOf (rows * cols) (BS.unpack pixels)
 >>>>>>> Work in progress
+
+  normalize :: Word8 -> Double
+  normalize = (/ 255) . fromIntegral
+  --normalize = (/ 0.3081) . (`subtract` 0.1307) . (/ 255) . fromIntegral
 
 -- | Reads a list of MNIST labels from a file and returns them.
 readMNISTLabels :: FilePath -> IO [Word8]
