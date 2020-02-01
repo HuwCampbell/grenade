bb98d46c536cc0c4a67b488654d6c7e2532b8408
jrp2014 <jrp2014@users.noreply.github.com>
2020-02-01 13:39:15 +0000

    limit samples from command line

--- examples/main/mnist.hs
+++ examples/main/mnist.hs
@@ -73,7 +73,6 @@ type MNIST =
 randomMnist :: MonadRandom m => m MNIST
 randomMnist = randomNetwork
 
-<<<<<<< HEAD
 convTest :: Int -> FilePath -> Maybe Int -> LearningParameters -> IO ()
 convTest iterations dataDir nSamples rate = do
   net0         <- randomMnist
@@ -83,28 +82,12 @@ convTest iterations dataDir nSamples rate = do
                             (dataDir </> "t10k-labels-idx1-ubyte.gz")
 
   foldM_ (runIteration (maybe trainData (`take` trainData) nSamples) validateData) net0 [1..iterations]
-=======
-convTest :: Int -> FilePath -> LearningParameters -> IO ()
-convTest iterations dataDir rate = do
-<<<<<<< HEAD
-  net0      <- randomMnist
-  trainData <- readMNIST (mkFilePath "train-images-idx3-ubyte.gz")
-                         (mkFilePath "train-labels-idx1-ubyte.gz")
-
-  validateData <- readMNIST (mkFilePath "t10k-images-idx3-ubyte.gz")
-                            (mkFilePath "t10k-labels-idx1-ubyte.gz")
-  foldM_ (runIteration trainData validateData) net0 [1 .. iterations]
 
  where
-
-  mkFilePath :: String -> FilePath
-  mkFilePath s = dataDir ++ '/' : s
->>>>>>> Work in progress
 
   trainEach rate' !network (i, o) = train rate' network i o
 
   runIteration trainRows validateRows net i = do
-<<<<<<< HEAD
     let trained' = foldl' (trainEach ( rate { learningRate = learningRate rate * 0.9 ^ i} )) net trainRows
     print trained'
 
@@ -140,7 +123,6 @@ main = do
 
     convTest iter dataDir nSamples rate
 
-
 -- Adapted from https://github.com/tensorflow/haskell/blob/master/tensorflow-mnist/src/TensorFlow/Examples/MNIST/Parse.hs
 -- Could also have used Data.IDX, although that uses a different Vector variant from that need for fromStorable
 readMNIST :: FilePath -> FilePath -> IO [(S ( 'D2 28 28), S ( 'D1 10))]
@@ -150,93 +132,10 @@ readMNIST iFP lFP = do
   return $ zip
     (fmap (fromMaybe (error "bad samples") . fromStorable) samples)
     (fromMaybe (error "bad labels") . oneHot . fromIntegral <$> labels)
-=======
-    let trained' = foldl'
-          (trainEach (rate { learningRate = learningRate rate * 0.9 ^ i }))
-          net
-          trainRows
-    let res =
-          fmap (\(rowP, rowL) -> (rowL, ) $ runNet trained' rowP) validateRows
-    let res' = fmap
-          (\(S1D label, S1D prediction) ->
-            (maxIndex (SA.extract label), maxIndex (SA.extract prediction))
-          )
-          res
-=======
-  net0         <- randomMnist
-  trainData    <- readMNIST (dataDir </> "train-images-idx3-ubyte.gz")
-                            (dataDir </> "train-labels-idx1-ubyte.gz")
-  validateData <- readMNIST (dataDir </> "t10k-images-idx3-ubyte.gz")
-                            (dataDir </> "t10k-labels-idx1-ubyte.gz")
-  -- reduce the number of training samples used from 60,000 to avoid running out of memory
-  foldM_ (runIteration (take 10000 trainData) validateData) net0 [1..iterations]
-
-    where
-  trainEach rate' !network (i, o) = train rate' network i o
-
-  runIteration trainRows validateRows net i = do
-    let trained' = foldl' (trainEach ( rate { learningRate = learningRate rate * 0.9 ^ i} )) net trainRows
->>>>>>> Convert gen-mnist example to use LeCun training data; works
-    print trained'
-
-    putStrLn "Checking..."
-    let res      = fmap (\(rowP,rowL) -> (rowL,) $ runNet trained' rowP) validateRows
-    let res'     = fmap (\(S1D label, S1D prediction) -> (maxIndex (SA.extract label), maxIndex (SA.extract prediction))) res
-    let matched   = length $ filter ((==) <$> fst <*> snd) res'
-    let total     = length res'
-    let matchedpc = fromIntegral matched / fromIntegral total * 100.0 :: Float
-    putStrLn $ "Iteration " ++ show i ++ ": " ++ show matched ++ " of " ++ show total ++ " (" ++ show matchedpc ++ "%)" 
-    return trained'
-
-data MnistOpts = MnistOpts FilePath Int LearningParameters
-
-mnist' :: Parser MnistOpts
-mnist' = MnistOpts <$> argument str (metavar "DATADIR")
-                   <*> option auto (long "iterations" <> short 'i' <> value 15)
-                   <*> (LearningParameters
-                       <$> option auto (long "train_rate" <> short 'r' <> value 0.01)
-                       <*> option auto (long "momentum" <> value 0.9)
-                       <*> option auto (long "l2" <> value 0.0005)
-                       )
-
-main :: IO ()
-main = do
-    MnistOpts dataDir iter rate <- execParser (info (mnist' <**> helper) idm)
-    putStrLn "Training convolutional neural network..."
-
-    convTest iter dataDir rate
-
-
--- Adapted from https://github.com/tensorflow/haskell/blob/master/tensorflow-mnist/src/TensorFlow/Examples/MNIST/Parse.hs
--- Could also have used Data.IDX, although that uses a different Vector variant from that need for fromStorable
-readMNIST :: FilePath -> FilePath -> IO [(S ( 'D2 28 28), S ( 'D1 10))]
-readMNIST iFP lFP = do
-  labels  <- readMNISTLabels lFP
-  samples <- readMNISTSamples iFP
-  return $ zip
-    (fmap (fromMaybe (error "bad samples") . fromStorable) samples)
-<<<<<<< HEAD
-    ((fromMaybe (error "bad labels") . oneHot . fromIntegral) <$> labels)
-
-<<<<<<< HEAD
-loadMNIST :: FilePath -> FilePath -> IO (Maybe [(S ( 'D2 28 28), S ( 'D1 10))])
-loadMNIST iFP lFP =  do
-  let labels  = MaybeT $ readMNISTLabels lFP
-  let samples = MaybeT $ fromIntegral . readMNISTSamples $ iFP
-  d <- MaybeT (fromStorable samples, (oneHot . fromIntegral) <$> labels)
-  return d
->>>>>>> Work in progress
-=======
->>>>>>> Adapt MNIST example to use original data files
-=======
-    (fromMaybe (error "bad labels") . oneHot . fromIntegral <$> labels)
->>>>>>> Convert gen-mnist example to use LeCun training data; works
 
 -- | Check's the file's endianess, throwing an error if it's not as expected.
 checkEndian :: Get ()
 checkEndian = do
-<<<<<<< HEAD
-<<<<<<< HEAD
   magic <- Serialize.getWord32be
   when (magic `notElem` ([2049, 2051] :: [Word32]))
     $ error "Expected big endian, but image file is little endian."
@@ -246,64 +145,17 @@ readMNISTSamples :: FilePath -> IO [V.Vector Double]
 readMNISTSamples path = do
   raw <- decompress <$> B.readFile path
   either fail ( return . fmap (V.map normalize) ) $ Serialize.runGetLazy getMNIST raw
-=======
-  magic <- getWord32be
-=======
-  magic <- Serialize.getWord32be
->>>>>>> Convert gen-mnist example to use LeCun training data; works
-  when (magic `notElem` ([2049, 2051] :: [Word32]))
-    $ error "Expected big endian, but image file is little endian."
-
--- | Reads an MNIST file and returns a list of samples.
-readMNISTSamples :: FilePath -> IO [V.Vector Double]
-readMNISTSamples path = do
-<<<<<<< HEAD
-  raw <- decompress <$> BS.readFile path
-<<<<<<< HEAD
-  return $ runGet getMNIST raw
->>>>>>> Work in progress
-=======
-  return . fmap (V.map normalize) $ runGet getMNIST raw
->>>>>>> Adapt MNIST example to use original data files
-=======
-  raw <- decompress <$> B.readFile path
-  either fail ( return . fmap (V.map normalize) ) $ Serialize.runGetLazy getMNIST raw
->>>>>>> Convert gen-mnist example to use LeCun training data; works
  where
   getMNIST :: Get [V.Vector Word8]
   getMNIST = do
     checkEndian
     -- Parse header data.
-<<<<<<< HEAD
-<<<<<<< HEAD
     cnt    <- fromIntegral <$> Serialize.getWord32be
     rows   <- fromIntegral <$> Serialize.getWord32be
     cols   <- fromIntegral <$> Serialize.getWord32be
     -- Read all of the data, then split into samples.
     pixels <- Serialize.getLazyByteString $ fromIntegral $ cnt * rows * cols
     return $ V.fromList <$> chunksOf (rows * cols) (B.unpack pixels)
-
-  normalize :: Word8 -> Double
-  normalize = (/ 255) . fromIntegral
-  -- There are other normalization functions in the literature, such as
-  -- normalize = (/ 0.3081) . (`subtract` 0.1307) . (/ 255) . fromIntegral
-  -- but we need values in the range [0..1] for the showShape' pretty printer
-=======
-    cnt    <- fromIntegral <$> getWord32be
-    rows   <- fromIntegral <$> getWord32be
-    cols   <- fromIntegral <$> getWord32be
-    -- Read all of the data, then split into samples.
-    pixels <- getLazyByteString $ fromIntegral $ cnt * rows * cols
-    return $ V.fromList <$> chunksOf (rows * cols) (BS.unpack pixels)
->>>>>>> Work in progress
-=======
-    cnt    <- fromIntegral <$> Serialize.getWord32be
-    rows   <- fromIntegral <$> Serialize.getWord32be
-    cols   <- fromIntegral <$> Serialize.getWord32be
-    -- Read all of the data, then split into samples.
-    pixels <- Serialize.getLazyByteString $ fromIntegral $ cnt * rows * cols
-    return $ V.fromList <$> chunksOf (rows * cols) (B.unpack pixels)
->>>>>>> Convert gen-mnist example to use LeCun training data; works
 
   normalize :: Word8 -> Double
   normalize = (/ 255) . fromIntegral
@@ -314,36 +166,13 @@ readMNISTSamples path = do
 -- | Reads a list of MNIST labels from a file and returns them.
 readMNISTLabels :: FilePath -> IO [Word8]
 readMNISTLabels path = do
-<<<<<<< HEAD
-<<<<<<< HEAD
   raw <- decompress <$> B.readFile path
   either fail return $ Serialize.runGetLazy getLabels raw
-=======
-  raw <- decompress <$> BS.readFile path
-  return $ runGet getLabels raw
->>>>>>> Work in progress
-=======
-  raw <- decompress <$> B.readFile path
-  either fail return $ Serialize.runGetLazy getLabels raw
->>>>>>> Convert gen-mnist example to use LeCun training data; works
  where
   getLabels :: Get [Word8]
   getLabels = do
     checkEndian
     -- Parse header data.
-<<<<<<< HEAD
-<<<<<<< HEAD
     cnt <- fromIntegral <$> Serialize.getWord32be
     -- Read all of the labels.
     B.unpack <$> Serialize.getLazyByteString cnt
-=======
-    cnt <- fromIntegral <$> getWord32be
-    -- Read all of the labels.
-    BS.unpack <$> getLazyByteString cnt
->>>>>>> Work in progress
-=======
-    cnt <- fromIntegral <$> Serialize.getWord32be
-    -- Read all of the labels.
-    B.unpack <$> Serialize.getLazyByteString cnt
->>>>>>> Convert gen-mnist example to use LeCun training data; works
-
--- runMNIST.sh
+++ runMNIST.sh
@@ -1,26 +1,33 @@
-#!/bin/bash -eu
->>>>>>> Convert gen-mnist example to use LeCun training data; works
+#!/bin/bash -eu 
+example=mnist
 stack=cabal
+
 trainset=train-images-idx3-ubyte.gz
 trainlab=train-labels-idx1-ubyte.gz
 testset=t10k-images-idx3-ubyte.gz
 testlab=t10k-labels-idx1-ubyte.gz
 url=http://yann.lecun.com/exdb/mnist
+
 datadir="data"
 args=""
-case $1 in
-  (-?*)
-    echo >&2 "Missing initial data directory argument"
-    exit 1;;
-  (*)
-    datadir=$1
-    shift
-    args="$@";;
-esac
-exec="$stack exec mnist --RTS -- $datadir $args +RTS -N -s"
+if [ $# -ge 1 ]
+then
+  case $1 in
+    (-?*)
+      echo >&2 "Missing initial data directory argument"
+      exit 1;;
+    (*)
+      datadir=$1
+      shift
+      args="$@";;
+  esac
+fi
+
+exec="$stack exec $example --RTS -- $datadir $args +RTS -N -s"
+
 if [ -f $datadir/$trainset ]
 then
-  $stack build mnist && $exec
+  $stack build $example && $exec
  else
   mkdir -p $datadir
   echo "Attempting to download MNIST data"
@@ -31,7 +38,7 @@ then
 
   if [ -f $datadir/$trainset ]
    then
-    $stack build mnist && $exec
+    $stack build $example && $exec
    else
     echo "$datadir/$trainset does not exist. Please download MNIST files to $datadir/"
   fi
